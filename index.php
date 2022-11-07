3944f69f8bc977f5cbb770014276742f12150e16
yeyo9 <kingyou5599@gmail.com>
2022-11-07 16:47:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+    <title>Mon CV</title>
+</head>
+
+<header class="header"> </header>
+<body>
+    
+    <section class="top-page">
+        
+
+            <h1>Ben Lahsen Youssef</h1>
+            
+            <img src="img/cage.jpg" alt="Salut"img/>
+    
+            <h2>Formations :</h2>
+            <nav class="nav">
+                <li>Bac littérature - lycée Jacques Chirac à Las Vegas - 2009</li>
+                <li>Brevet des collèges - Collège Cool à New York - 2000</li>
+                <li>Brevet Primaire - Ecole primaire à Tokyo - 1990</li>
+                <li>Doctorat en rien - Maternelle Jean Claude Van Damme à Paris - 1989,
+                    <a href="https://www.fscfrance.fr/wp-content/uploads/2019/12/diplome-doctorat-vierge-14avril2021.jpg">voici mon diplôme</a>.
+                    </li>
+            </nav>
+
+            <h2> Expérience professionnel</h2>
+            <li>Nike - chef de rayon à Lille - 2009</li>
+            <li>Ferrari - chef de projet à New York - 2010</li>
+            <li>Bugatti - Pilote professionnel à Paris - 2009</li>
+
+            <h2> Compétences</h2>
+            <li>Ponctuel</li>
+            <li>Sérieux</li>
+            <li>Professionnel</li>
+            
+            <h2> Hobbies</h2>
+            <li>Lecture</li>
+            <li>Chasse et pêche</li>
+            <li>Dormir</li>
+
+
+
+       
+        
+        
+
+
+    
+    </section>
+
+
+
+</body>
+
+<footer class="footer"> Copyright © 2022 
+
+<h2> Mentions Légales
+<li>Qui suis-je ?</li>
+            <li>CGV</li>
+            <li>Politique de confidentialité</li></h2>
+
+
+</footer>
+
+
+
+
+</html>
